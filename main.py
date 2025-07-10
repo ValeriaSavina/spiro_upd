d94c8fc0fbcb6255ea032cb1e89555c43eeb71f2
Valeria Savina <72984665+ValeriaSavina@users.noreply.github.com>
2025-07-10 22:06:49 +0400

    Update main.py
    удалили лишние импорты х2

--- main.py
+++ main.py
@@ -1,13 +1,7 @@
 import pandas as pd
 import streamlit as st
 import numpy as np
-from sklearn.feature_selection import RFE
-from sklearn.tree import DecisionTreeClassifier
-from sklearn.metrics import accuracy_score
-from sklearn.metrics import classification_report
-from sklearn.model_selection import GridSearchCV
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import LabelEncoder
 from sklearn.ensemble import RandomForestClassifier
 
 
